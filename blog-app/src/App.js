c3cc7f0e67b1a6b94386d002300742c55c1c5dc0
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2024-07-29 15:51:34 +0700

    Add Carousel (for fun)

--- blog-app/src/App.js
+++
@@ -1,9 +0,0 @@
-import { Typography, Card, Button } from "@material-tailwind/react";
-
-export default function App() {
-  return (
-    <div className="relative grid min-h-[100vh] w-screen p-8">
-      <Button className="text-red-500">Click</Button>
-    </div>
-  );
-}
--- blog-app/src/index.jsx
+++ blog-app/src/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-
+// import App from "./App";
+import Carousel1 from "./components/Carousel/Carousel";
 import { ThemeProvider } from "@material-tailwind/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <Carousel1 />
     </ThemeProvider>
   </React.StrictMode>,
 );
