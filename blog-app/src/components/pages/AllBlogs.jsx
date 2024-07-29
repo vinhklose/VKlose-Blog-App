48b91903592c66180923a3a21037e4393a3e4cb1
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2024-07-29 16:21:39 +0700

    set up app structure

---
+++ blog-app/src/components/pages/AllBlogs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function AllBlogs() {
+    return (
+        <div>Home</div>
+    )
+}
--- blog-app/src/index.jsx
+++ blog-app/src/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
-import Carousel1 from "./components/Carousel/Carousel";
+import App from "./App";
 import { ThemeProvider } from "@material-tailwind/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <Carousel1 />
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
 );
