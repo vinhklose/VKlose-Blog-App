58757b5a72529834d0bf03343ee73175d6e95d44
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2024-07-29 13:19:28 +0700

    Set up the basics.

---
+++ blog-app/src/App.js
@@ -0,0 +1,9 @@
+import { Typography, Card, Button } from "@material-tailwind/react";
+
+export default function App() {
+  return (
+    <div className="relative grid min-h-[100vh] w-screen p-8">
+      <Button>Click</Button>
+    </div>
+  );
+}
